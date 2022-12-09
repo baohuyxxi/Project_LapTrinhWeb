08fab3359faeea1f597408c569c1336932b85b30
Đặng Văn Tuấn <102749812+tuandangvan@users.noreply.github.com>
2022-12-09 21:35:36 +0700

    pull main

--- Project_LTWeb/src/main/java/Controller/HomeController.java
+++ Project_LTWeb/src/main/java/Controller/HomeController.java
@@ -16,11 +16,8 @@ public class HomeController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		RequestDispatcher rd =  req.getRequestDispatcher("/views/home.jsp"); 
-=======
-		RequestDispatcher rd =  req.getRequestDispatcher("/views/login.jsp"); 
->>>>>>> a83c682ef8e61e89f9a7bf350ce1dc7f97e93ddf
+
 		rd.forward(req, resp);
 	}
 
