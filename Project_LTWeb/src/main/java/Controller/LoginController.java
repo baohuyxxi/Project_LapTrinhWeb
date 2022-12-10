66e144f78082b6edb44df07c069e9793d3b2265a
duydang1207 <dang01635441895@gmail.com>
2022-12-10 22:12:52 +0700

    commit

--- Project_LTWeb/src/main/java/Controller/LoginController.java
+++ Project_LTWeb/src/main/java/Controller/LoginController.java
@@ -81,12 +81,11 @@ public class LoginController extends HttpServlet {
 		} else {
 			resp.sendRedirect(req.getContextPath() + "/login");
 		}
-			}
-		}
-		
-		catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
-}
+			
+		}catch(
 
+	Exception e)
+	{
+		// TODO: handle exception
+	}
+}}
